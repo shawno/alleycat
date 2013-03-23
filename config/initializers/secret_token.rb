d12cc6aebde41f2404913deb3fc7357910595f60
Shawn O'Neill <soneill83@gmail.com>
2013-03-23 10:38:30 -0700

    initial rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alleycat::Application.config.secret_token = '2cd89e99de0eab9674f0b2135978a2b12913a4db282908926ca865a273183a52d0399e46cd46515eec2ff61c7584eae4debe1b456172e577f770045a723a9fe8'
